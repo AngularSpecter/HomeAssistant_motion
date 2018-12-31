8ba2f1513dc2ad498e2c516a7851ec2f084ec90d
Matthew <mjbeals@mtu.edu>
2018-12-31 17:08:25 -0500

    Create HA_motion_off.sh

---
+++ HA_motion_off.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+   curl -X POST \
+    -H "Content-Type: application/json" \
+    -d '{"payload": "{\"status\" :\"NO MOTION\"}", "topic": "home/downstairs/playroom/sensor/motion", "retain": "True"}' \
+    http://<HA_IP>:8123/api/services/mqtt/publish
